4f217f97e3e3d84feb84c21af586dd9f3ded9368
Natalia Dumondin <nataliadumondin@gmail.com>
2024-06-14 11:56:13 -0300

    JS de experiencias

--- js/contacto.js
+++ js/contacto.js
@@ -1,2 +1,2 @@
 alert(`¡No olvides completar todos los campos!`)
-
+//¿Se puede modificar el JS de bootstrap para que el sistema arroje un error cuando el usuario completa los campos de forma errónea.//
