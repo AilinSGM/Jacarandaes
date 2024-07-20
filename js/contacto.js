4bd3633f61b025072a6966af290ff4589a0b24d7
AranzaBGR <aranzabgr7@gmail.com>
2024-07-19 23:08:58 -0300

    solo comillas

--- js/contacto.js
+++ js/contacto.js
@@ -9,55 +9,60 @@ let formulario = document.getElementById('form.comunicacion');
 formulario.addEventListener('submit', function(event){
     event.preventDefault();
     let inputNombre = document.querySelector('input.name');
-if (inputNombre.value == ''){
+if (inputNombre.value == ""){
     alert('El campo nombre tiene que estar completo')
-} else if (inputNombre.value.length <3) {
+} else if (inputNombre.value.length < 3) {
     alert ('El campo nombre debe tener al menos 3 caracteres')
 };
 
     let inputSurname = document.querySelector('input.surname');
-if (inputSurname.value == ''){
+if (inputSurname.value == ""){
     alert('El campo apellido tiene que estar completo')
-} else if (inputSurname.value.length <3) {
+} else if (inputSurname.value.length < 3) {
     alert ('El campo apellido debe tener al menos 3 caracteres')
 };
 
     let inputEmail = document.querySelector('input.email');
-if (inputEmail.value == ''){
+if (inputEmail.value == ""){
     alert('El campo correo electrónico tiene que estar completo')
 } else if (inputEmail.value.includes('@')) {
     alert ('El email no es correcto')
 }
 
     let inputPhone = document.querySelector('input.phone');
-if (inputPhone.value == ''){
+if (inputPhone.value == ""){
     alert('El campo teléfono tiene que estar completo')
-} else if (inputPhone.value.length <8) {
+} else if (inputPhone.value.length < 8) {
     alert ('El campo teléfono debe tener al menos 8 caracteres')
 }
+//aca ver de sacar las flechitas
 
 let inputMesagge = document.querySelector('input.mesagge');
-if (inputMesagge.value == ''){
+if (inputMesagge.value == ""){
     alert('El campo mensaje tiene que estar completo')
-} else if (inputMesagge.value.length <50) {
-    alert ('El campo teléfono debe tener al menos 50 caracteres')
+} else if (inputMesagge.value.length < 50) {
+    alert ('El campo mensaje debe tener al menos 50 caracteres')
 }
 });
 
 formulario.addEventListener('submit', function(event){ 
-    let errores = []
-    let inputNombre = document.querySelector('input.name');
-    if (inputNombre.value == ''){
+   let errores = []
+ let inputNombre = document.querySelector('input.name');
+    if (inputNombre.value == ""){
         errores.push('El campo nombre tiene que estar completo')
-    } else if (inputNombre.value.length <3){
+    } else if (inputNombre.value.length < 3){
         errores.push('El campo nombre debe tener al menos 3 caracteres')
     }
   
-    if (errores.length >0){
-        event.preventDefault();
-    {errores.push('Los campos deben estar completos')}}
+    if (errores.length > 0){
+        event.preventDefault();}
 
 let ulErrores = document.querySelector('div.errores ul');
 for (let i = 0; i < errores.length; i++){
     ulErrores.innerHTML += '<li>' + errores[i] + '</li>'}
 });
+
+inputNombre.addEventListener('focus', () => {
+    inputNombre.style.backgroundColor = 'blue'
+})
+
--- js/experiencias.js
+++ js/experiencias.js
@@ -1,4 +1,3 @@
 let inscripcion = prompt("¿Tenés alguna discapacidad motriz?")
-inscripcion = si ? "¡Animate e inscribite en nuestros talleres gratuitos!" : "Compartí esta información a quien lo necesite.";
-console.log(inscripcion);
+inscripcion = si ? "¡Animate e inscribite en nuestros talleres gratuitos!" : "Compartí esta información a quien lo necesite.";;
 // no nos funciona el 2do cartel cuando el usuario responde "si"//
