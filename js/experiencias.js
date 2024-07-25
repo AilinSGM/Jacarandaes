6d2dc60b40c6f26db8681ab87a8f05ad4244029d
Natalia Dumondin <nataliadumondin@gmail.com>
2024-07-24 23:36:50 -0300

    Página de registro e intento de validacion de usuarios

--- js/experiencias.js
+++ js/experiencias.js
@@ -1,4 +1,4 @@
-let inscripcion = prompt("¿Tenés alguna discapacidad motriz?")
+/*let inscripcion = prompt("¿Tenés alguna discapacidad motriz?")
 
 //inscripcion = si ? "¡Animate e inscribite en nuestros talleres gratuitos!" : "Compartí esta información a quien lo necesite.";;
 //no nos funciona el 2do cartel cuando el usuario responde "si"
@@ -7,8 +7,8 @@ let inscripcion = prompt("¿Tenés alguna discapacidad motriz?")
 /* Prueba 2
 inscripcion = si*/
 
-if (inscripcion == "si") {
+/*if (inscripcion == "si") {
     alert ("¡Animate e inscribite en nuestros talleres gratuitos!");
 } else {
     alert ("Compartí esta información a quien lo necesite.");
-} 
+}
--- js/index.js
+++ js/index.js
@@ -36,7 +36,8 @@ let db = JSON.parse (dbUsuarios)
 let emailIngresado = prompt ('Ingrese su email')
 let passwordIngresada = prompt ('Ingrese su contraseña') */
 
-if(nombreIngresado !== null && passwordIngresada !== null && emailIngresado !== null){
+
+/*if(nombreIngresado !== null && passwordIngresada !== null && emailIngresado !== null){
     for(let i = 0; i < db.length ; i++){
         if(db[i].email === emailIngresado){
             if(db[i].password === passwordIngresada){
@@ -62,7 +63,64 @@ if(usuarioEncontrado !== true) {
     }
     db.push(usuarioPorGuardar);
     usuario = usuarioPorGuardar;
-}
+}*/
 
+window.addEventListener('load', function(){
+    let listaDeErrores = document.querySelector('#errores');
+    let formulario = document.getElementById('#formLogin');
+    let inputEmail = document.querySelector('#emailLogin');
+    let inputPassword = document.querySelector('#contrasenaLogin');
+    let saludo = document.querySelector('#saludoDeBienvenida');
 
+   // console.log(listaDeErrores, formulario, inputEmail, inputPassword)
+       
+    formulario.addEventListener('submit', function(event){
+
+        event.preventDefault();
+        let errores = [];
+
+        if (inputEmail.value == ''){
+            errores.push('El campo correo electrónico que estar completo')            
+        }
+
+        if(inputPassword.value == ''){
+            errores.push('El campo contraseña tiene que estar completo')
+        }
+        let emailEncontrado = false;
+        for(let i = 0; i < db.length ; i++){
+            if(db[i].email === inputEmail.value){
+                emailEncontrado = true;
+                if(db[i].password === inputPassword.value){
+                    nombreDelUsuario = db [i].nombre;
+                     
+                    const usuario = {
+                        nombre: db [i].nombre,
+                        email: db [i].email,
+                        password: db [i].password,
+                    }
+                    db.push(usuario)
+                    localStorage.setItem('usuario', JSON.stringify(usuario));
+
+                    saludo.innerHTML = `¡${db [i].nombre}, le damos la bienvenida!`
+                    sessionStorage.setItem('usuario', JSON.stringify(usuario));
+              } else{
+                    errores.push('La contraseña no coincide')
+                }
+            }   
+        }     
+
+        if(emailEncontrado === false){
+            errores.push('El correo electrónico no se encuentra registrado')
+        }
+        if (errores.length > 0) {
+            for(let i = 0; i < errores.length; i++){
+                listaDeErrores.innerHTML += `<li> ${errores[i]} </li>`
+            }
+
+            if(emailEncontrado === false){
+                alert('Debe registrarse en la sección de registro')
+            }
+        }
+    })
+})
 
